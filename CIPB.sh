44596c1efe1943aea8f234c387bb4583334597f2
root <coldsyntax@hotmail.com>
2025-07-11 22:13:14 -0400

    Added a very basic way to randomize pin stacks for all difficulties

--- CIPB.sh
+++ CIPB.sh
@@ -32,37 +32,54 @@ set_lock()
 	# Medium 4-6, 1-2 security pins of the same type
 	# Hard 5-6, 3-6 security pins of randomized type and check to see if 5 pins 6 sec happens
 	# Add an options be challenge to a selected pick from both geno and eche a turning position (Bottom for rakes only)
-	# set_lock() min_pins min_sec max_sec random_sec
-	t=$1
-	num_of_pins=$(("$RANDOM"%6))
-	min_sec=$2
-	max_sec=$3
-	if [[ max_sec -gt 0 && min_sec -gt 0 ]]; then
-		num_of_sec=$(("$RANDOM"%$max_sec+$min_sec))
-	else
-		num_of_sec=0;
+	# set_lock() min_pins security_count random_sec
+	case $1 in
+		3)
+			num_of_pins=$(("$RANDOM"%4+$1))
+			;;
+		4)
+			num_of_pins=$(("$RANDOM"%3+$1))
+			;;
+		5)
+			num_of_pins=$(("$RANDOM"%2+$1))
+			;;
+	esac
+
+	case $2 in
+		0)
+			num_of_sec=0
+			;;
+		1)
+			num_of_sec=$(("$RANDOM"%2+$2))
+			;;
+		3)
+			num_of_sec=$(("$RANDOM"%4+$2))
+			;;
+	esac
+
+	if [[ "$num_of_pins" -lt "$num_of_sec" ]]; then
+		num_of_sec=$(("$num_of_pins"))
 	fi
-	random_sec=$4
+
+	random_sec=$3
 
 	declare -a pins=()
-	for ((i=0; i < $"num_of_pins"; i++))
+	for ((i=0; i < "$num_of_pins"; i++))
 	do
-		if [[ "$num_of_sec" -gt 0 && "$num_of_pins"-"$i" -lt "$num_of_sec" ]]; then
-			if [ "$RANDOM" % "2" -eq "0" ]; then
+		if [[ "$num_of_sec" -gt 0 && $(("$num_of_pins"-"$i")) -ge "$num_of_sec" ]]; then
+			if [[ $(("$RANDOM" % 2)) -eq 0 && $(("$num_of_pins"-"$i")) -gt "$num_of_sec" ]]; then
 				pins+=("Standard")
 			else
 				pins+=("Special")
 				let num_of_sec--
-				# let num_of_pins--
 			fi
 		else
 			pins+=("Standard")
 		fi
 
-		echo "pin $i:" ${pins[$i]}
-		echo "sec" $num_of_sec
-		echo "Number of pins left"$num_of_pins-$i
-		echo "Loop remain"$i
+		echo "pin $(($i+1)):" ${pins[$i]}
+		#echo "sec " $num_of_sec
+		#echo "Number of pins left "$(("$num_of_pins"-"$i"-1))
 	done
 }
 
@@ -73,16 +90,20 @@ difficulty()
 	case $1 in
 		[Ee]* )
 			echo "Choosing difficulty mode: Easy"
-			set_lock 3 0 0 false;;
+			set_lock 3 0 false
+			;;
 		[Mm]* )
 			echo "Choosing difficulty mode: Middle"
-			set_lock 4 1 2 false;;
+			set_lock 4 1 false
+			;;
 		[Hh]* )
 			echo "Choosing difficulty mode: Hard"
-			set_lock 5 3 6 true;;
+			set_lock 5 3 true
+			;;
 		*)
 			echo "This is just plainly wrong"
-			menu_opt;;
+			menu_opt
+			;;
 	esac
 }
 
